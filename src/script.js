088f09c681f445918e6166b1421112e330c193e8
AlexFern17 <123132428+AlexFern17@users.noreply.github.com>
2024-07-01 18:31:11 -0500

    Imported axios and integrated the API from SheCodes

--- src/script.js
+++ src/script.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter(".poem", {
-    strings: ["123"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#instructions");
+  let apiKey = "097041f091d320fa0aa8fbb43t7o1142";
+  let prompt = `User instructions: Generate a poem in Spanish about ${instructionsInput.value}`;
+  let context =
+    "You are a poem expert who loves to write short poems. Your purpose is to generate a simple 4 line poem in basic HTML, and separate each line with a <br/>.  Be sure to follow the user instructions.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
